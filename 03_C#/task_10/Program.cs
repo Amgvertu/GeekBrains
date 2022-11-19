11cd4309ad6bf0bda9685b195f64424b0de9591f
amgvertu <amgvertu@gmail.com>
2022-11-20 00:22:02 +0700

    Task_10 added

---
+++ 03_C#/task_10/Program.cs
@@ -0,0 +1,24 @@
+﻿// // Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+//               и на выходе показывает вторую цифру этого числа. Выполнить с помощью 
+//               числовых операций (целочисленное деление, остаток от деления).
+
+// // 456 -> 5
+// // 782 -> 8
+// // 918 -> 1
+
+Console.Clear();
+int num;
+do
+{
+    Console.Write("Введите трехзначное число: ");
+    num = Convert.ToInt32(Console.ReadLine());
+    if (IfThreeDigit(num) == false) Console.WriteLine("Вы ввели не трехзначное число, попробуйте еще раз!!!");
+} while (IfThreeDigit(num) == false);
+
+int secondDigit = (num / 10) % 10;
+Console.WriteLine($"Вторая цифра числа --> {secondDigit}");
+
+Boolean IfThreeDigit(int number)
+{
+    return (number > 99) & (number < 1000);
+}
