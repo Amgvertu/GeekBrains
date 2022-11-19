0137657c700b08d90d6fdc57df543366baad2a64
amgvertu <amgvertu@gmail.com>
2022-11-20 03:52:30 +0700

    Task_19  add

---
+++ 03_C#/Practice/task_19-1/Program.cs
@@ -0,0 +1,46 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+// Универсальный способ для любого количества чисел
+bool flag = false;
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 0) num = -num;
+
+for (int i = 1; i <= HowMuchDigit(num) / 2; i++)
+    if (((num / InDegree(HowMuchDigit(num) - i)) % InDegree(i - i + 1)) == ((num % InDegree(i)) / InDegree(i - 1))) flag = true;
+    else flag = false;
+
+if (flag == true) Console.WriteLine("--> Да");
+else Console.WriteLine("--> Нет");
+
+
+
+// Методы
+int HowMuchDigit(int exp)
+{
+    int i = 1;
+    while (exp / 10 != 0)
+    {
+        exp = exp / 10;
+        i = i + 1;
+    }
+    return i;
+}
+
+
+int InDegree(int degree)
+{
+    int num = 1;
+    for (int i = 1; i < degree + 1; i++)
+    {
+        num = num * 10;
+    }
+    return num;
+}
---
+++ 03_C#/Practice/task_19/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+// По заданию
+
+int num;
+Console.Clear();
+
+do
+{
+    Console.Write("Введите число: ");
+    num = Convert.ToInt32(Console.ReadLine());
+    if (HowMuchDigit(num) != 5) Console.WriteLine("Вы ввели не пятизначное число, попробуйте еще раз!!!");
+} while (HowMuchDigit(num) != 5);
+if (num < 0) num = -num;
+if ((num / 10000 == num % 10) & ((num / 1000) % 10 == (num / 10) % 10)) Console.WriteLine(" --> Да");
+else Console.WriteLine(" --> Нет");
+
+// Методы
+int HowMuchDigit(int exp)
+{
+    int i = 1;
+    while (exp / 10 != 0)
+    {
+        exp = exp / 10;
+        i = i + 1;
+    }
+    return i;
+}
