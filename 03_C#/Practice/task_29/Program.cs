5b320e8308f76969333cf181ca3170aedec20c21
amgvertu <amgvertu@gmail.com>
2022-11-27 23:54:30 +0700

    task_29 added

---
+++ 03_C#/Practice/task_29/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив 
+//            из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+Console.Clear();
+int[] array = FillArray(0, 999);
+PrintArray(array);
+
+// Методы
+// Заполнение массива
+int[] FillArray(int min, int max)
+{
+    Random rnd = new Random();
+    int[] arr = new int[8];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max);
+    }
+
+    return arr;
+}
+
+// Вывод массива
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i == arr.Length - 1) Console.Write($"{arr[i]}");
+        else Console.Write($"{arr[i]}, ");
+    }
+    Console.Write("]");
+
+}
