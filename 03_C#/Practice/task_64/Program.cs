e26d26d43025b910efb619b8e94dfd85c5863699
amgvertu <amgvertu@gmail.com>
2022-12-17 23:33:04 +0700

    task_66 added

--- 03_C#/Practice/task_64/Program.cs
+++ 03_C#/Practice/task_64/Program.cs
@@ -5,7 +5,7 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-Console.Write("Введите число N: ");
+Console.Write("Введите натуральное число N: ");
 int n = Convert.ToInt32(Console.ReadLine());
 Nat(n);
 
--- 03_C#/Practice/task_66/Program.cs
+++ 03_C#/Practice/task_66/Program.cs
@@ -3,3 +3,23 @@
 
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите отрезок чисел от M до N");
+Console.Write("Введите натуральное число M : ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число N : ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum;
+if (m<n) sum = SumNat(0,m,n);
+else sum = SumNat(0, n,m);
+Console.WriteLine($"{sum}");
+//Методы
+int SumNat(int sum, int m, int n)
+{
+    if (n<m) return sum;
+    else
+    {
+        sum = sum + n;
+        return SumNat(sum, m,n - 1);
+    } 
+}
