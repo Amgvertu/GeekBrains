274d4cd6dd4785d4df79b52b4fbb663fb6be2ccd
amgvertu <amgvertu@gmail.com>
2023-03-10 11:44:15 +0700

    03_20

--- 06_Python/HomeWork/Lesson_3/03_20.py
+++ 06_Python/HomeWork/Lesson_3/03_20.py
@@ -10,3 +10,23 @@
 
 # ноутбук
 #     12
+
+mark_1 = ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'S', 'T', 'R','А', 'В', 'Е', 'И', 'Н', 'О', 'Р', 'С', 'Т']
+mark_2 = ['D', 'G', 'Д', 'К', 'Л', 'М', 'П', 'У']
+mark_3 = ['B', 'C', 'M', 'P', 'Б', 'Г', 'Ё', 'Ь', 'Я']
+mark_4 = ['F', 'H', 'V', 'W', 'Y', 'Й', 'Ы']
+mark_5 = ['K', 'Ж', 'З', 'Х', 'Ц', 'Ч']
+mark_8 = ['J', 'X', 'Ш', 'Э', 'Ю']
+mark_10 = ['Q', 'Z', 'Ф', 'Щ', 'Ъ']
+
+dict_all = {1:mark_1, 2: mark_2, 3: mark_3, 4: mark_4, 5:mark_5, 8: mark_8, 10: mark_10}
+sum = 0
+print("Введите слово: ", sep="", end="")
+word = input()
+for i in range(0,len(word)):
+    for key in dict_all:
+        for j in range(0, len(dict_all[key])):
+            if (dict_all[key][j] == word[i].upper()):
+                sum +=int(key)
+
+print(sum)
